f0dd187762b4af1d4f42f331afcc2e3af402e63b
harryitc <cuongharryit@gmail.com>
2026-07-28 12:35:08 +0700

    feat(calendar): add form view extensions and weekly schedule menu

--- custom-addons/aidt_calendar/__manifest__.py
+++ custom-addons/aidt_calendar/__manifest__.py
@@ -7,6 +7,7 @@
     'data': [
         'security/ir.model.access.csv',
         'security/aidt_calendar_rules.xml',
+        'views/calendar_event_views.xml',
     ],
     'installable': True,
     'application': True,
--- custom-addons/aidt_calendar/tests/__init__.py
+++ custom-addons/aidt_calendar/tests/__init__.py
@@ -2,4 +2,5 @@ from . import test_scaffold
 from . import test_calendar_event
 from . import test_calendar_security
 from . import test_meeting_task
+from . import test_calendar_views
 
