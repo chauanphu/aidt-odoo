4108c2bd39c84c074973392d49109ea382d9296b
AIDT Developer <dev@aidt.vn>
2026-08-04 10:01:32 +0700

    fix(aidt_sign): add related_sudo=True for all hr.employee private fields on res.users to fix preferences AccessError

--- custom-addons/aidt_sign/models/res_users.py
+++ custom-addons/aidt_sign/models/res_users.py
@@ -5,4 +5,21 @@ class ResUsers(models.Model):
 
     digital_signature_img = fields.Binary(string='Ảnh chữ ký tay tươi')
     certificate_ids = fields.One2many('aidt.sign.certificate', 'owner_id', string='Chứng thư số cá nhân')
+
+    # Bổ sung related_sudo=True cho tất cả các trường thông tin cá nhân liên kết tới hr.employee
+    # để người dùng mở trang My Preferences không bị lỗi Access Error do phân quyền nhóm hr.group_hr_user của hr.employee
+    private_street = fields.Char(related='employee_id.private_street', readonly=False, related_sudo=True)
+    private_street2 = fields.Char(related='employee_id.private_street2', readonly=False, related_sudo=True)
+    private_city = fields.Char(related='employee_id.private_city', readonly=False, related_sudo=True)
+    private_state_id = fields.Many2one(related='employee_id.private_state_id', readonly=False, related_sudo=True)
+    private_zip = fields.Char(related='employee_id.private_zip', readonly=False, related_sudo=True)
+    private_country_id = fields.Many2one(related='employee_id.private_country_id', readonly=False, related_sudo=True)
+    private_phone = fields.Char(related='employee_id.private_phone', readonly=False, related_sudo=True)
+    private_email = fields.Char(related='employee_id.private_email', readonly=False, related_sudo=True)
+    km_home_work = fields.Integer(related='employee_id.km_home_work', readonly=False, related_sudo=True)
+    emergency_contact = fields.Char(related='employee_id.emergency_contact', readonly=False, related_sudo=True)
+    emergency_phone = fields.Char(related='employee_id.emergency_phone', readonly=False, related_sudo=True)
+    visa_expire = fields.Date(related='employee_id.visa_expire', readonly=False, related_sudo=True)
+    additional_note = fields.Text(related='employee_id.additional_note', readonly=False, related_sudo=True)
+    barcode = fields.Char(related='employee_id.barcode', readonly=False, related_sudo=True)
     pin = fields.Char(related='employee_id.pin', readonly=False, related_sudo=True)
